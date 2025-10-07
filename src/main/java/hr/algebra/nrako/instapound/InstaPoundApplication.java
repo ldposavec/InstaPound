adcaa6dcff9abff644e48330e99b1208dde76c55
ldposavec <lposavec@algebra.hr>
2025-10-07 23:24:17 +0200

    first commit

---
+++ src/main/java/hr/algebra/nrako/instapound/InstaPoundApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.nrako.instapound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaPoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaPoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/algebra/nrako/instapound/InstaPoundApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.nrako.instapound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaPoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
