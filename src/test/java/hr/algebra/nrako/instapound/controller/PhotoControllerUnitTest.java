d78fc54c43b21d1a95ad98829511918db0075499
ldposavec <lposavec@algebra.hr>
2026-07-01 20:10:37 +0200

    Tests 2/2

---
+++ src/test/java/hr/algebra/nrako/instapound/controller/PhotoControllerUnitTest.java
@@ -0,0 +1,4 @@
+package hr.algebra.nrako.instapound.controller;
+
+public class PhotoControllerUnitTest {
+}
---
+++ src/test/java/hr/algebra/nrako/instapound/service/storage/CloudinaryStorageStrategyImplTest.java
@@ -0,0 +1,4 @@
+package hr.algebra.nrako.instapound.service.storage;
+
+public class CloudinaryStorageStrategyImplTest {
+}
